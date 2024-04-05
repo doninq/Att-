002578b0da106ca24bbf441b3920c5f728f754c7
Nikita Donin <doninq@yandex.ru>
2024-04-05 13:38:22 +0300

    рабочая программа

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+       for(int j = 0; j < matrix.GetLength(1); j++)
+       {        
+           string? name = Console.ReadLine();
+           
+       }
+       */
+       string[] StrMass = {"#5efS", "M", "Tu", "Red", "T", "Tertery"};
+
+
+       for(int i = 0; i < StrMass.Length; i++)
+       {        
+           //StrMass[i] = Console.WriteLine();
+           Console.Write("[" + StrMass[i] + "]");
+       }
+       Console.WriteLine("");
+
+       for(int i = 0; i < StrMass.Length; i++)
+       {        
+            if(StrMass[i].Length>3){
+                //StrMass[i] = Console.WriteLine();
+                Console.Write("[" + StrMass[i] + "]");
+            }
+       }
